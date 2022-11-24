7f7d4e38c6461c8024804e4d96145b9a53f3551f
Mag1strVark <molot2101@gmail.com>
2022-11-24 18:05:32 +0500

    second

--- src/components/Assortment/Assortment.js
+++ src/components/Assortment/Assortment.js
@@ -7,14 +7,14 @@ const SubPage = (props) => {
     const List = props.listData.map(item => <Box title={item.title} text={item.text}/>)
 
     return (
-        <div className={s.container}>
+        <section className={s.container} id='sort'>
             <div className={s.title}>
                 <h3>Ассортимент</h3>
             </div>
             <div className={s.list}>
                 {List}
             </div>
-        </div>
+        </section>
     )
 }
 
